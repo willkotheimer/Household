9e214c0e7ff9ea66dfe4f00ef4fcd2d2c48c443d
wkotheimer <wkotheimer@gmail.com>
2021-05-30 22:03:23 -0500

    Added user repo and api

--- HouseHoldApp/HouseHoldApp/Models/UserHousehold.cs
+++ HouseHoldApp/HouseHoldApp/Models/UserHousehold.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 
 namespace HouseHoldApp.Models
 {
-    public class Users
+    public class UserHousehold
     {
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Email { get; set; }
-        public string FirebaseKey { get; set; }
+        public string firebaseKey { get; set; }
+        public int HouseholdId { get; set; }
     }
 }
